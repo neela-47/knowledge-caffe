09c24313332cf507689ccf1484ea911b9b423518
nila akter <akternila1147@gmail.com>
2024-10-25 11:29:00 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='text-4xl font-bold'>
+             <h1>Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
